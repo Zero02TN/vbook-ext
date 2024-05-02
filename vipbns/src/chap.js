35e4d1029e5f9b06252371d78ff321bd881eb6ef
PGV <46308607+nhocconsr@users.noreply.github.com>
2024-05-02 14:02:18 +0700

    fix vipbns

--- vipbns/src/chap.js
+++ vipbns/src/chap.js
@@ -2,11 +2,11 @@ function execute(url) {
     let chapId = url.split('|')[1];
     let browser = Engine.newBrowser();
     browser.launch(url.split('|')[0], 5000);
-    browser.close();
     let response = fetch('https://bachngocsach.net.vn/api/chapter/'+chapId);
+    browser.close();
     if (response.ok) {
         let data = response.json();
-        if (!data || data.chapter.price > 0) {
+        if (!data || data.chapter.bought === false) {
             return Response.error('Đây là chương mất tiền hoặc bạn chưa đăng nhập nick vào web bằng vbook. Đăng nhập rồi load lại nhé!');
         } else {
             let content = data.chapter.content.replace(/<!-- (.*?) -->/gm, '')
@@ -15,4 +15,5 @@ function execute(url) {
             return Response.success(content);
         }
     }
+    return null;
 }
