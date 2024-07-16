215add00657b87647ebe60f20faf4ead8f361bd3
PGV <nhocconsr@gmail.com>
2024-07-16 22:09:28 +0700

    newcode

--- mangaread/src/top.js
+++ mangaread/src/top.js
@@ -1,24 +1,23 @@
 function execute(url, page) {
     if (!page) page = '0';
-    const doc = Http.post('https://www.mangaread.org/wp-admin/admin-ajax.php').params({
-        "action": "madara_load_more",
-        "page": page,
-        "template": "madara-core/content/content-archive",
-        "vars[orderby]": "meta_value_num",
-        "vars[paged]": "1",
-        "vars[posts_per_page]": "38",
-        "vars[post_type]": "wp-manga",
-        "vars[post_status]": "publish",
-        "vars[meta_key]": "_latest_update",
-        "vars[sidebar]": "right",
-        "vars[manga_archives_item_layout]": "default"
-    }).html()
-
-
+    const doc = fetch('https://www.mangaread.org/wp-admin/admin-ajax.php',{
+        method : "POST",
+        body : {
+            "action": "madara_load_more",
+            "page": page,
+            "template": "madara-core/content/content-archive",
+            "vars[orderby]": "meta_value_num",
+            "vars[paged]": "1",
+            "vars[posts_per_page]": "38",
+            "vars[post_type]": "wp-manga",
+            "vars[post_status]": "publish",
+            "vars[meta_key]": "_latest_update",
+            "vars[sidebar]": "right",
+            "vars[manga_archives_item_layout]": "default"
+        }
+    }).html();
     const data = [];
-
     var el = doc.select(".page-listing-item .page-item-detail")
-
     for (var i = 0; i < el.size(); i++) {
         var e = el.get(i)
         data.push({
@@ -26,7 +25,7 @@ function execute(url, page) {
             link: e.select("h3.h5 a").first().attr("href"),
             cover: e.select(".item-thumb img").first().attr("src"),
             description: e.select(".list-chapter > div:nth-child(1) a").text(),
-            host: "https://truyenz.info"
+            host: "https://www.mangaread.org"
         })
     }
 
