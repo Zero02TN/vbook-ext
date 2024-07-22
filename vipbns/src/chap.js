7af0691c46fca7ef46469ce711176cce8780cffa
PGV <nhocconsr@gmail.com>
2024-07-22 11:41:13 +0700

    fix getchuong bns

--- vipbns/src/chap.js
+++ vipbns/src/chap.js
@@ -1,13 +1,8 @@
 load('config.js');
 function execute(url) {
     let chapId = url.split('|')[1];
-    if (B_TOKEN) {
-        let newtoken = fetch('https://bachngocsach.net.vn/api/auth/session',{
-            method: 'GET',
-            headers: {
-                cookie: '__Secure-next-auth.session-token='+B_TOKEN
-            }
-        }).json().accessToken
+    if (B_TOKEN === "Nhocconsr") {
+        let newtoken = fetch('https://bachngocsach.net.vn/api/auth/session').json().accessToken
         let response = fetch('https://ngocsach.com/api/chapter-app/'+chapId, {
             method: 'GET',
             headers: {
@@ -22,7 +17,7 @@ function execute(url) {
                     .replace(/\n/g, '<br>');
                 return Response.success(content);
             } else {
-                return Response.success("Đã có token! Đây là chương mất tiền hoặc bạn chưa thêm mã tài khoản vào ext bằng vbook.<br>Thêm mã như sau vào <br> let CONFIG_TOKEN = 'Nhập mã token từ tài khoản của bạn'");
+                return Response.success("Đã có token! Đây là chương mất tiền hoặc bạn chưa thêm mã và đăng nhập tài khoản vào bằng vbook.'");
             }
         }
     } else {
@@ -30,7 +25,7 @@ function execute(url) {
         if (response.ok) {
             let data = response.json();
             if (!data || data.price > 0) {
-                return Response.success("Đây là chương mất tiền hoặc bạn chưa thêm mã tài khoản vào ext bằng vbook.<br>Thêm mã như sau vào <br>let CONFIG_TOKEN = 'Nhập mã token từ tài khoản của bạn'");
+                return Response.success("Đây là chương mất tiền hoặc bạn chưa thêm mã và đăng nhập tài khoản vào bằng vbook.");
             } else {
                 let content = data.content.replace(/<!-- (.*?) -->/gm, '')
                     .replace(/<p(.*?)>(.*?)<?p>/g, '')
