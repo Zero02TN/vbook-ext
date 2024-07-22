110aec0078bc52acd4c594f2aa6df72eac7db396
PGV <nhocconsr@gmail.com>
2024-07-22 09:31:37 +0700

    fix bns

--- vipbns/src/chap.js
+++ vipbns/src/chap.js
@@ -2,10 +2,16 @@ load('config.js');
 function execute(url) {
     let chapId = url.split('|')[1];
     if (B_TOKEN) {
+        let newtoken = fetch('https://bachngocsach.net.vn/api/auth/session',{
+            method: 'GET',
+            headers: {
+                cookie: '__Secure-next-auth.session-token='+B_TOKEN
+            }
+        }).json().accessToken
         let response = fetch('https://ngocsach.com/api/chapter-app/'+chapId, {
             method: 'GET',
             headers: {
-                authorization: 'Bearer ' + B_TOKEN,
+                authorization: 'Bearer ' + newtoken,
             }
         });
         if (response.ok) {
@@ -16,7 +22,7 @@ function execute(url) {
                     .replace(/\n/g, '<br>');
                 return Response.success(content);
             } else {
-                return Response.success("Đây là chương mất tiền hoặc bạn chưa thêm mã tài khoản vào ext bằng vbook.<br>Thêm mã như sau vào <br> let CONFIG_TOKEN = 'Nhập mã token từ tài khoản của bạn'");
+                return Response.success("Đã có token! Đây là chương mất tiền hoặc bạn chưa thêm mã tài khoản vào ext bằng vbook.<br>Thêm mã như sau vào <br> let CONFIG_TOKEN = 'Nhập mã token từ tài khoản của bạn'");
             }
         }
     } else {
