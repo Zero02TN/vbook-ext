960e05e857f8f68b90e11e31f293587bb8498270
PGV <nhocconsr@gmail.com>
2024-07-30 07:10:11 +0700

    remove fastscans

--- fastscans/src/home.js
+++
@@ -1,6 +0,0 @@
-function execute() {
-    return Response.success([
-        { title: "Cập Nhật", input: "updatedOnUtc", script: "gen.js" },
-        { title: "Phổ Biến", input: "views", script: "gen.js" },
-    ]);
-}
