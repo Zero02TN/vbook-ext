d83f51e93effd4cbdb434b74dd48c721845fbd72
PGV <nhocconsr@gmail.com>
2024-09-17 20:17:13 +0700

    fix lxmanga

--- lxhentai/src/chap.js
+++ lxhentai/src/chap.js
@@ -4,11 +4,11 @@ function execute(url) {
     var doc = fetch(url).html();
     var el = doc.select(".text-center img");
     var imgs = [];
-    for (var i = 0; i < el.size() - 1; i++) {
-        var link = el.get(i).attr("src");
+    el.forEach(e =>{
+        var link = e.attr("data-src") ;
         if (link.indexOf("banners") < 0) {
             imgs.push(link.trim())
         }
-    }
+    })
     return Response.success(imgs);
 }
