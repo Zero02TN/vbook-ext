691e676725df896e5941af8fafd5b0fafd956878
PGV <nhocconsr@gmail.com>
2024-09-23 19:11:11 +0700

    uplx

--- lxhentai/src/chap.js
+++ lxhentai/src/chap.js
@@ -2,7 +2,7 @@ load('config.js');
 function execute(url) {
     url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
     var doc = fetch(url).html();
-    var el = doc.select(".text-center img");
+    var el = doc.select("#image-container");
     var imgs = [];
     el.forEach(e =>{
         var link = e.attr("data-src") ;
