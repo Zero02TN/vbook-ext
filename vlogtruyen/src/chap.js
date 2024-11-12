6e823bba04aad0c5f961de19968906e5a9c1c769
PGV <nhocconsr@gmail.com>
2024-11-12 18:01:48 +0700

    fix vlogtruyen

--- vlogtruyen/src/chap.js
+++ vlogtruyen/src/chap.js
@@ -2,15 +2,18 @@ load('config.js');
 function execute(url) {
     url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
     let response = fetch(url);
-    if(response.ok){
+    if (response.ok) {
         let doc = response.html();
         let el = doc.select(".comicDetails img");
-        Console.log(el)    
+        console.log(el);    
         let data = [];
         for (var i = 0; i < el.size(); i++) {
             var e = el.get(i);
             let img = e.attr("src");
-            img.indexOf('gif') > 0 ? '' : data.push(img);   
+            if (img.includes('/images/')) {
+                continue;
+            }
+            data.push(img);   
         }
         return Response.success(data);
     }
