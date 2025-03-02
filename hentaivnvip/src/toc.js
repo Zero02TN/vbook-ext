3c91eb1f07e6c7b8349778d594cf0d16b0294562
ZeroTNV <144619528+Zero02TN@users.noreply.github.com>
2025-03-03 01:05:20 +0700

    Add files via upload

--- hentaivnvip/src/toc.js
+++ hentaivnvip/src/toc.js
@@ -1,28 +1,16 @@
 load('config.js');
-
 function execute(url) {
-    const list = [];
-    let page = 1;
-    while (true) {
-        // Trang 1 sử dụng url gốc, các trang sau nối thêm /page/{page}
-        let pageUrl = (page === 1) ? url : url + '/page/' + page;
-        var response = fetch(pageUrl);
-        var doc = response.html();
-        
-        // Lấy danh sách chương từ phần tử chứa danh sách chương (giả sử selector không đổi)
-        var chapters = doc.select("ul.version-chap > li > a");
-        // Nếu không có chương nào thì thoát vòng lặp
-        if (chapters.length === 0) break;
-        
-        chapters.forEach(chapter => {
-            list.push({
-                name: chapter.text(),  // nếu chapter đã là thẻ <a> thì dùng text() trực tiếp
-                url: chapter.attr("href"),
-                host: BASE_URL
-            });
-        });
-        page++;
+    url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
+    let doc = fetch(url).html();
+    let el = doc.select("ul.box-list-chapter li").select("a");
+    let data = [];
+    for (let i = el.size() - 1; i >= 0; i--) {
+        var e = el.get(i);
+        data.push({
+            name: e.text(),
+            url: e.attr("href"),
+            host: BASE_URL
+        })
     }
-    // Đảo ngược danh sách nếu cần (theo logic của code cũ)
-    return Response.success(list.reverse());
+    return Response.success(data);
 }
