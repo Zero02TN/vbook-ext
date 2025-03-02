9e3cbe362f9215df208502157b24bcc6d21046ad
ZeroTNV <144619528+Zero02TN@users.noreply.github.com>
2025-03-03 03:15:48 +0700

    Add files via upload

--- hentaivnvip/src/toc.js
+++ hentaivnvip/src/toc.js
@@ -1,16 +1,16 @@
 load('config.js');
 function execute(url) {
-    url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
-    let doc = fetch(url).html();
-    let el = doc.select("ul.box-list-chapter li").select("a");
-    let data = [];
-    for (let i = el.size() - 1; i >= 0; i--) {
-        var e = el.get(i);
-        data.push({
-            name: e.text(),
+    var doc = fetch(url+'/ajax/chapters/',{
+        method: "POST"
+    }).html();
+    var el = doc.select("ul.version-chap > li > a")
+    const list = [];
+    el.forEach(e =>{
+        list.push({
+            name: e.select("a").text(),
             url: e.attr("href"),
             host: BASE_URL
         })
-    }
-    return Response.success(data);
+    })
+    return Response.success(list.reverse());
 }
