7123430864724d5c614362af44ca7d2629bf9641
PGV <46308607+nhocconsr@users.noreply.github.com>
2025-03-12 21:14:03 +0700

    fx voz

--- vozer/src/toc-page.js
+++ vozer/src/toc-page.js
@@ -2,11 +2,11 @@ load('config.js');
 function execute(url) {
     let data = [];
     let doc = fetch(url).html();
-    const anchors = doc.select('a[href*="pagechap="]:not([rel])');
+    const anchors = doc.select('a[href*="page_chap="]:not([rel])');
     let lastNumber = -1;
     anchors.forEach(anchor => {
         const href = anchor.attr('href'); 
-        const match = href.match(/pagechap=(\d+)/);
+        const match = href.match(/page_chap=(\d+)/);
         if (match) {
             const pageNumber = parseInt(match[1], 10);
             lastNumber = Math.max(lastNumber, pageNumber);    
