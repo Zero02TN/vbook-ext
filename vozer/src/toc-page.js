1710ba9b5f0b3ef1840c79ded49873ef793dbc57
PGV <46308607+nhocconsr@users.noreply.github.com>
2025-06-03 14:39:11 +0700

    fix vozer

--- vozer/src/toc-page.js
+++ vozer/src/toc-page.js
@@ -2,11 +2,14 @@ load('config.js');
 function execute(url) {
     let data = [];
     let doc = fetch(url).html();
-    const anchors = doc.select('a[href*="page_chap="]:not([rel])');
+    const anchors = doc.select('[role=navigation] a[href*="pagechap="]');
+    if(anchors.length === 0) {
+        return Response.success([url]);
+    }
     let lastNumber = -1;
     anchors.forEach(anchor => {
         const href = anchor.attr('href'); 
-        const match = href.match(/page_chap=(\d+)/);
+        const match = href.match(/pagechap=(\d+)/);
         if (match) {
             const pageNumber = parseInt(match[1], 10);
             lastNumber = Math.max(lastNumber, pageNumber);    
@@ -15,6 +18,5 @@ function execute(url) {
     for (let i = 1; i <= lastNumber; i++) {
         data.push(url + "?pagechap=" + i);
     }
-    // Trả về số cuối cùng
     return Response.success(data);
 }
